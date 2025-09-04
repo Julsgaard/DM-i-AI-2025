98590ee53ec0e81f6dabf125946209c60993b969
Kristian Julsgaard <100912975+Julsgaard@users.noreply.github.com>
2025-09-04 10:52:45 +0200

    Update example.py

--- race-car/example.py
+++ race-car/example.py
@@ -51,7 +51,7 @@ vx = 10 # The velocity x from the sensor - Only used for printing
 vx_band = 0.15 # Dead zone around target_vx
 base_target_vx = 10.05 # Start slow
 max_target_vx = 999 # The maximum target VX
-ramp_per_tick = 0.1 # vx gained per tick
+ramp_per_tick = 0.5 # vx gained per tick
 ramp_ticks = 0 # Only counts when NOT steering
 current_target_vx = base_target_vx  # Computed each call
 
